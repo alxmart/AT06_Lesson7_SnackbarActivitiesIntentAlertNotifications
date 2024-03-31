940aa50bd4f9b949c7280ef84c3c43cc09514839
alxmart <alxmartinez@gmail.com>
2024-03-31 13:42:41 -0300

    Testing Snackbar using different configurations.

--- app/src/main/java/com/luizafmartinez/at06_lesson7_snackbaractivitiesintentalertnotifications/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/at06_lesson7_snackbaractivitiesintentalertnotifications/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.luizafmartinez.at06_lesson7_snackbaractivitiesintentalertnotifications
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 import com.luizafmartinez.at06_lesson7_snackbaractivitiesintentalertnotifications.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,13 +19,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         setContentView(binding.root)
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    fun MgSnack(view: View) {
+        Snackbar.make(findViewById(R.id.sendMessage),
+            "Your email has been sent succesfully",
+            Snackbar.LENGTH_LONG)
+            .setAction("OK") { }.setActionTextColor(Color.RED).show()
+    }
+
+
+
 }
